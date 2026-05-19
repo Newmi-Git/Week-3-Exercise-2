69feaf11c33b25b1205014e8fbd495667589432a
Yaghya_Abdul_GIT <yaghyaabdul1006@gmail.com>
2026-05-19 09:58:34 +0200

    Add files via upload

---
+++ operation-types.js
@@ -0,0 +1,29 @@
+// TODO: Create a variable called number1 and store any number in it
+// TODO: Create a variable called number2 and store a different number in it
+// TODO: Convert number1 to a string and store it in a new variable
+// TODO: Add number1 and number2 and store the result in a variable called sum
+// TODO: Multiply number1 and number2 and store the result in a variable called product
+// TODO: Print all results to the console
+
+let number1 = 20;
+let number2 = 30;
+
+console.log(number1);
+console.log(number1);
+
+console.log(number2);
+console.log(number2);
+
+let stringNumOne = String(number1);
+console.log(stringNumOne)
+
+let sum = number1 + number2;
+console.log(sum)
+
+let product = number1 * number2;
+console.log(product)
+
+
+
+
+
